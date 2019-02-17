a62871b573f6716e12acdd396a5b7f2eb170d2af
fernando <fernando_lopezc@live.com>
2019-02-17 12:58:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ programmer_hash =
      #   :known_for => "Ruby",
      #    :languages => ["LISP", "C"]
      # }
-programmer_hash[:yukihiro_matsumoto]
+programmer_hash[:yukihiro_matsumoto] = [:known_for]
 end
 
 def changing_alan
