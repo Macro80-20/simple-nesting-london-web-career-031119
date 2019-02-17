59ad9333d98e241ae9103b515f2ca3ce7e559dac
fernando <fernando_lopezc@live.com>
2019-02-17 12:57:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,13 +60,9 @@ def dennis_ritchies_language
 
 end
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
-	programmer_hash =
+
+programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -81,8 +77,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-
+     # add the following information to the top level of programmer_hash
+     # :yukihiro_matsumoto => {
+     #   :known_for => "Ruby",
+     #    :languages => ["LISP", "C"]
+     # }
+programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
